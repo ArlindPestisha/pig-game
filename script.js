1350df57b17671fc1fa53aeed7942dcdfe423ab0
ArlindPestisha <55292668+ArlindPestisha@users.noreply.github.com>
2020-11-25 10:44:46 +0100

    starting to selct and make the logic of the game

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+// Selecting #id-s and .classes
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice')
+
+// Starting condition
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add('hidden')
+
+//To Do:
+// 1. Look to the Flow chart to
+// 2. Roll the dice make the function 
+// 3. Add the score to the current player.
