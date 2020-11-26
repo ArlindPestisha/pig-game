9e7056afc22d2b9a1f9a63193ec92946d7a83b4a
ArlindPestisha <55292668+ArlindPestisha@users.noreply.github.com>
2020-11-26 08:13:25 +0100

    building the dynamic part to change from current player to next player when ever we score 1 and also to change the UI

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const diceEl = document.querySelector('.dice')
 const btnNewGame = document.querySelector('.btn--new')
 const btnRollDice = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 console.log(btnNewGame);
 
 // Starting condition
@@ -15,6 +17,8 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
+const scores = [0, 0]
+let activePlayer = 0
 let currentScore = 0
 
 btnRollDice.addEventListener('click', function () {
@@ -28,9 +32,14 @@ btnRollDice.addEventListener('click', function () {
     if (randomDice !== 1) {
         // Add dice to current score
         currentScore += randomDice
-        current0El.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
     } else {
         // Switch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        currentScore = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
     }
 })
 //To Do:
